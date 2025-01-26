f4ca42aeef7787088d61feb662f333acbb05e6e9
Madhavi8074 <madhavi738685@gmail.com>
2025-01-26 19:39:53 +0530

    Add files via upload

---
+++ Patterns1.java
@@ -0,0 +1,12 @@
+public class Patterns1{
+    public static void main(String[] args){
+        System.out.println("First Pattern: ");
+                int n=5;
+                for (int i=1;i<=n;i++){
+                    for(int j=1;j<=i;j++){
+                          System.out.print("* ");
+                    }
+                    System.out.println();
+                }
+    }
+}
